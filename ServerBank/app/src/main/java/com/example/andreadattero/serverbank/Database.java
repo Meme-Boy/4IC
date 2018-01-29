ed8b1915642ecd5d1ac3f2ee9eebdf3bc20ac229
unknown <dattero.andrea@gmail.com>
2018-01-29 17:06:08 +0100

    Tris e Safe added

---
+++ ServerBank/app/src/main/java/com/example/andreadattero/serverbank/Database.java
@@ -0,0 +1,63 @@
+package com.example.andreadattero.serverbank;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Database extends AppCompatActivity {
+    public static EditText text;
+    private Button send;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+        send = findViewById(R.id.send);
+        text=findViewById(R.id.text);
+        send.setOnClickListener((View view)->{
+            try {
+                Socket client = new Socket("79.54.6.193",8080);
+                DataOutputStream dout = new DataOutputStream(client.getOutputStream());
+                String testo=text.getText() + "";
+                if(!testo.substring(0,3).contains("get")) {
+                    dout.write(testo.getBytes(), 0, testo.getBytes().length);
+                    dout.flush();
+                    dout.close();
+                }
+                else{
+                    if(testo.substring(5,8).contains("all")){
+                        dout.write("get all".getBytes(),0,"get all".getBytes().length);
+                        dout.flush();
+                        dout.close();
+                    }
+                    if(testo.charAt(5)=='{'){
+                        String arg="";
+                        for(int i=0; testo.charAt(i)=='}';i++){
+                            arg+=testo.charAt(i);
+                        }
+                        new Thread(new ListenForResponse(client,this)).start();
+                        dout.write(("get "+arg).getBytes(),0,("get "+arg).getBytes().length);
+                        dout.flush();
+                        dout.close();
+                    }
+                }
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
+    public void deleteAllPasswordsSaved(View view){
+        Context context = this;
+        PreferenceManager.getDefaultSharedPreferences(context).edit().clear().commit();
+    }
+
+}
---
+++ ServerBank/app/src/main/java/com/example/andreadattero/serverbank/ListenForResponse.java
@@ -0,0 +1,40 @@
+package com.example.andreadattero.serverbank;
+
+import android.app.Activity;
+import android.provider.ContactsContract;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by Andrea Dattero on 28/01/2018.
+ */
+
+class ListenForResponse implements Runnable {
+    Socket client;
+    Activity activity;
+    public ListenForResponse(Socket client, Activity activity) {
+        this.client=client;
+        this.activity=activity;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                DataInputStream din = new DataInputStream(client.getInputStream());
+                String testo=din.readUTF();
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Database.text.setText(testo);
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
