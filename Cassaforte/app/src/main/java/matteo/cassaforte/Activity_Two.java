6cc16cc764c085438f41df0e007bf2262546c515
Matteo Fusati <fusa53@gmail.com>
2018-01-31 17:46:24 +0100

    Aggiunta Cassaforte

---
+++ Cassaforte/app/src/main/java/matteo/cassaforte/Activity_Two.java
@@ -0,0 +1,15 @@
+package matteo.cassaforte;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by Matteo on 30/01/2018.
+ */
+
+public class Activity_Two {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
