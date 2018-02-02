4a4df5c983103585af4aa9f2c0e4a44a206d8176
Pietro Cappellato <pietro.cap@outlook.com>
2018-02-02 18:33:24 +0100

    Update

--- Tris/app/src/main/java/com/example/zen/tris/MainActivity.java
+++ Tris/app/src/main/java/com/example/zen/tris/MainActivity.java
@@ -1,21 +1,25 @@
-package com.example.obzentish.tris;
+package com.example.zen.tris;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button start;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        start = findViewById(R.id.start);
     }
 
-    public void start(View view){
-        startActivity(new Intent(this,Match.class));
+    public void PVPNames(View v){
+        Intent i = new Intent(this, PVPNames.class);
+        startActivity(i);
+    }
+
+    public void PVCNames(View v){
+        Intent i = new Intent(this, PVCNames.class);
+        startActivity(i);
     }
 }
